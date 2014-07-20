b703d3a2e80a59620ee907aca6bfbb853fcb7464
cmcnulty <cmcnulty@kznf.com>
2014-07-19 21:58:36 -0500

    Update simple.js

--- tests/simple.js
+++ tests/simple.js
@@ -2,15 +2,10 @@ var supergenpass = require('../bcryptgenpass-lib');
 
 exports.testSimple = function(test){
     var data = [
-        ['w9UbG0NEk7', 'test', 'example.com'],
-        ['sJfoZg3nU8', 'test', 'example.com', { method: 'sha512' }],
-        ['aC81', 'test', 'example.com', { length: 4, method: 'sha512' }],
-        ['vBKDNdjhhL6dBfgDSRxZxAAA', 'test', 'example.com', { length: 24, method: 'md5' }],
-        ['sJfoZg3nU8y32EyHFRlSY08u', 'test', 'example.com', { length: 24, method: 'sha512' }],
-        ['zPQSNhTzs9fS', 'test', 'https://www.google.com/', { length: 12, secret: 'test' }],
-        ['q8ZWYccWDt', 'test', 'https://www.google.com/', { removeSubdomains: false, method: 'sha512' }],
-        ['aRFG84Gim9', 'test', 'example.co.uk'],
-        ['hSF8nTst4A', 'test', 'example.gov.ac']
+        ['=`lMr7nmMN', 'test', 'example.com'],
+        ['6qM]', 'test', 'example.com', { length: 4, cost: 10 }],
+        [';`$fq;+Ff$=^kQ*<H_Gb10S^', 'test', 'example.com', { length: 24, cost: 11 }],
+        ['6t]M=93-:)F*', 'test', 'example.com', { length: 12, secret: 'test' }]
     ];
 
     data.forEach(function(c){
