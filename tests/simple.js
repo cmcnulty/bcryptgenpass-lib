84c16000adfd09d858589215b837e4948ad33750
cmcnulty <cmcnulty@kznf.com>
2014-08-03 14:41:26 -0500

    test asyncness

--- tests/simple.js
+++ tests/simple.js
@@ -9,11 +9,29 @@ test('simple comparisons', function (test) {
         ['iaL0Wgb7WAg+@U$iwUe8fGy*', 'test', 'example.com', { length: 24, costFactor: 4 }],
         ['ggB&Kw&RO8i5', 'test', 'example.com', { length: 12, secret: 'test', costFactor: 4 }]
     ];
-
+	
 	test.plan(data.length);
 
     data.forEach(function(c){
         test.equal(supergenpass(c[1], c[2], c[3]), c[0]);
+    });
+
+});
+
+test('asynchronous comparisons', function (test) {
+	
+	var callback = function( expected, actual ){
+		test.equal(expected, actual);
+	};	
+	var aSyncData = [
+        ['test', 'example.com', { costFactor: 4, callback: callback.bind( null, 'iaL0Wgb7WAg+') } ],
+		['test', 'example.com', { costFactor: 4, callback: callback.bind( null, 'w[9P'), length: 4 } ]
+	];
+
+	test.plan(aSyncData.length);
+
+    aSyncData.forEach(function(c){
+        supergenpass(c[0], c[1], c[2]);
     });
 
 });
